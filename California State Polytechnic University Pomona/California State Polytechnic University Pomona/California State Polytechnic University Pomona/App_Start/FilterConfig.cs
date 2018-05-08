70e35073653dcfca5e3e41301b8856fafeb7e1ff
Nawaf Alsufiani <nawafh1986@hotmail.com>
2018-05-08 08:28:56 -0700

    addMore

---
+++ California State Polytechnic University Pomona/California State Polytechnic University Pomona/California State Polytechnic University Pomona/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace California_State_Polytechnic_University_Pomona
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
